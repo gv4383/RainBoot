b569b57e62a5a1ea2a75d7122b90288b01476c5f
gv4383 <gregory.vargas18@gmail.com>
2022-01-27 22:13:17 -0600

    Initial Commit

---
+++ RainBoot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RainBoot
+//
+//  Created by Goyo Vargas on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RainBoot/RainBootApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainBootApp.swift
+//  RainBoot
+//
+//  Created by Goyo Vargas on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RainBootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

64c27064b99a5639bf97f95673eef8a8f5fc1855
gv4383 <gregory.vargas18@gmail.com>
2022-02-02 08:46:30 -0600

    Create basic layout for city name and date

--- RainBoot/Views/DashboardView.swift
+++ RainBoot/Views/DashboardView.swift
@@ -13,15 +13,31 @@ struct DashboardView: View {
     
     @EnvironmentObject var locationManager: LocationManager
     
-    var weatherManager = WeatherManager()
+    // TODO: Move this to view model
+    var currentDate: String {
+        Date().formatted(.dateTime.month().day().hour().minute())
+    }
     
     var body: some View {
-        Text("Dashboard View")
+        VStack() {
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("\(weather.name),")
+                        .bold().font(.title)
+                    
+                    Text(currentDate)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+        }
     }
 }
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView(weather: previewWeather)
+            .preferredColorScheme(.dark)
     }
 }
